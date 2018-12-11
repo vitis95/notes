786d3d1882ac2cc30c2051c883f6f3b0a802ef20
DESKTOP-DMTEPSK\utente <ivandevita1995@gmail.com>
2018-12-11 11:39:20 +0100

    lezione 24

--- javascript/base_test_code/script.js
+++ javascript/base_test_code/script.js
@@ -1,9 +1,8 @@
-function sommato (y,x) {
-    return sommato(y,x);
-}
-console.log(sommato(y,x))
-function somma (x = 2, y = 3) {
-var lopp = x + y;
-return lopp
-}
-console.log(somma(x = 2,y = 3))
+function contaCaratteri (name){
+  return name.length
+  }
+  contaCaratteri('marco');
+  contaCaratteri('alessio');
+  contaCaratteri('francesco');
+  contaCaratteri('martina');
+  console.log(contaCaratteri)
