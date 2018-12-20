dd63d9c513c853725c29c5fe00e8131d3b90cdd5
DESKTOP-DMTEPSK\utente <ivandevita1995@gmail.com>
2018-12-20 10:41:32 +0100

    lezione 33 e altre cose boh

--- javascript/base_test_code/script.js
+++ javascript/base_test_code/script.js
@@ -1,8 +1,18 @@
-function contaCaratteri (name){
-  return name.length
-  }
-  contaCaratteri('marco');
-  contaCaratteri('alessio');
-  contaCaratteri('francesco');
-  contaCaratteri('martina');
-  console.log(contaCaratteri)
+var saluto = "Buongiorno";
+var visualizzaSaluti;
+function saluta(persone) {
+	var nomeCognome;
+	var saluti = [];
+	for (var i in persone) {
+		nomeCognome = persone[i].nome + " " + persone[i].cognome;
+		saluti.push(function() {
+			console.log(saluto + " " + nomeCognome);
+		});
+	}
+	return saluti;
+}
+visualizzaSaluti = saluta([{nome: "Mario", cognome: "Rossi"},
+                           {nome: "Marco", cognome: "Neri"}]);
+for (var i in visualizzaSaluti) {
+	visualizzaSaluti[i]();
+}
