7101e933ef625edb73f076940b96d0f818e95cf4
DESKTOP-DMTEPSK\utente <ivandevita1995@gmail.com>
2018-12-21 11:20:35 +0100

    lezione 37

--- javascript/base_test_code/script.js
+++ javascript/base_test_code/script.js
@@ -1,18 +1,8 @@
-var saluto = "Buongiorno";
-var visualizzaSaluti;
-function saluta(persone) {
-	var nomeCognome;
-	var saluti = [];
-	for (var i in persone) {
-		nomeCognome = persone[i].nome + " " + persone[i].cognome;
-		saluti.push(function() {
-			console.log(saluto + " " + nomeCognome);
-		});
-	}
-	return saluti;
-}
-visualizzaSaluti = saluta([{nome: "Mario", cognome: "Rossi"},
-                           {nome: "Marco", cognome: "Neri"}]);
-for (var i in visualizzaSaluti) {
-	visualizzaSaluti[i]();
-}
+function contaCaratteri (ciao){
+  return ciao.substr(4, 1);
+  }
+ var a = contaCaratteri('marco');       
+ b = contaCaratteri('alessio');
+  c = contaCaratteri('francesco');
+ d = contaCaratteri('martina');
+  console.log(a,b,c,d)
