e4c0fc3f1c60eacff75f17070d39c921bb72ab6f
Ivan De Vita <vitis@new-host.station>
2019-01-07 12:20:07 +0100

    tanta roba

--- javascript/base_test_code/script.js
+++
@@ -1,8 +0,0 @@
-function contaCaratteri (ciao){
-  return ciao.substr(4, 1);
-  }
- var a = contaCaratteri('marco');       
- b = contaCaratteri('alessio');
-  c = contaCaratteri('francesco');
- d = contaCaratteri('martina');
-  console.log(a,b,c,d)
