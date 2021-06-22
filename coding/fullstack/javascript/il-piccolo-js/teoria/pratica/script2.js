0caa927ec229c980ac361669f0cefaf755afcdbd
vitis95 <37616023+vitis95@users.noreply.github.com>
2021-06-23 01:03:20 +0200

    test con js

--- coding/fullstack/javascript/il-piccolo-js/teoria/pratica/script2.js
+++ coding/fullstack/javascript/il-piccolo-js/teoria/pratica/script2.js
@@ -12,6 +12,7 @@ const persona = {
 }
 
 var nome = persona.nome;
+var età = persona.età;
 var etàUtente = 0; 
 console.log("Ciao il mio nome è: ", nome);
 function inputEtà(){
@@ -19,11 +20,17 @@ function inputEtà(){
     console.log("prova", document.getElementById("myText").value);
     var etàUtente = document.getElementById("myText").value;
     console.log("La differenza delle nostre età è: ");
+    var diffEtà = persona.calcolaDiffEtà(etàUtente); 
     console.log(persona.calcolaDiffEtà(etàUtente));
+    document.getElementById('diffEtà').innerHTML=diffEtà;
 
 }
 
 // console.log("età utente", etàUtente);
+
+
+document.getElementById('età').innerHTML=età;
+document.getElementById('diffEtà').innerHTML=diffEtà;
 
 
 
